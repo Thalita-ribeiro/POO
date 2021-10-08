ede0b423cec4c886d80aad60b992b653e217dc52
Thalita Ribeiro <sribeiro.thalita@gmail.com>
2021-10-07 22:58:47 -0300

    Criando classe de comida

---
+++ Comida.java
@@ -0,0 +1,16 @@
+public class Comida {
+    boolean comFome = true;
+
+    public void seEstiverComFomePedirComida() {
+        if (comFome) {
+            System.out.println("Pedir comida");
+        } else {
+            System.out.println("Sem fome");
+        }
+    }
+
+    public static void main(String[] args) {
+        Comida comida = new Comida();
+        comida.seEstiverComFomePedirComida();
+    }
+}
