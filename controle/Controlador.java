5f55a53449835839df8e0661620478f678113e28
Thalita Ribeiro <sribeiro.thalita@gmail.com>
2021-10-14 20:39:48 -0300

    Encapsulamento

---
+++ controle/Controlador.java
@@ -0,0 +1,23 @@
+package controle;
+
+public interface Controlador {
+    public abstract void ligar();
+
+    public abstract void desligar();
+
+    public abstract void abrirMenu();
+
+    public abstract void fecharrMenu();
+
+    public abstract void maisVolume();
+
+    public abstract void menosVolume();
+
+    public abstract void ligarMudo();
+
+    public abstract void desligarMudo();
+
+    public abstract void play();
+
+    public abstract void pause();
+}
---
+++ controle/ControleRemoto.java
@@ -0,0 +1,88 @@
+package controle;
+
+public class ControleRemoto implements Controlador {
+    private int volume;
+    private boolean ligado;
+    private boolean tocando;
+
+    public ControleRemoto() {
+        this.volume = 50;
+        this.ligado = false;
+        this.tocando = false;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public boolean isLigado() {
+        return ligado;
+    }
+
+    public void setLigado(boolean ligado) {
+        this.ligado = ligado;
+    }
+
+    public boolean isTocando() {
+        return tocando;
+    }
+
+    public void setTocando(boolean tocando) {
+        this.tocando = tocando;
+    }
+
+    @Override
+    public void ligar() {
+
+    }
+
+    @Override
+    public void desligar() {
+
+    }
+
+    @Override
+    public void abrirMenu() {
+
+    }
+
+    @Override
+    public void fecharrMenu() {
+
+    }
+
+    @Override
+    public void maisVolume() {
+
+    }
+
+    @Override
+    public void menosVolume() {
+
+    }
+
+    @Override
+    public void ligarMudo() {
+
+    }
+
+    @Override
+    public void desligarMudo() {
+
+    }
+
+    @Override
+    public void play() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+}
+
