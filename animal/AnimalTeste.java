efb84aeeafda993ef849b1d124366bce1c155dcd
Thalita Ribeiro <sribeiro.thalita@gmail.com>
2021-10-24 20:34:43 -0300

    Trabalhando com sobrecarga

--- animal/AnimalTeste.java
+++ animal/AnimalTeste.java
@@ -9,35 +9,40 @@ public class AnimalTeste {
         Peixe peixe = new Peixe();
         Ave ave = new Ave();
 
-        mamifero.setPeso(80.3);
-        mamifero.setIdade(7);
-        mamifero.setMembros("4");
-        mamifero.setCorPelo("Preto");
-        mamifero.alimentar();
+//        mamifero.setPeso(80.3);
+//        mamifero.setIdade(7);
+//        mamifero.setMembros("4");
+//        mamifero.setCorPelo("Preto");
+//        mamifero.alimentar();
 
         cachorro.enterrarOsso();
         cachorro.emitirSom();
 
-        lobo.emitirSom();
-
-        reptil.setPeso(5);
-        reptil.setIdade(3);
-        reptil.setMembros("4");
-        reptil.setCorEscama("Laranja");
-        reptil.emitirSom();
-
-        peixe.setPeso(2);
-        peixe.setIdade(1);
-        peixe.setMembros("7 nadadeira");
-        peixe.setCorEscama("Azul marinho");
-        peixe.locomover();
-        peixe.soltarBolha();
-
-        ave.setPeso(12);
-        ave.setIdade(4);
-        ave.setMembros("4");
-        ave.setCorPena("Vermelho");
-        ave.locomover();
-        ave.fazerNinho();
+        cachorro.reagir(true);
+        cachorro.reagir("Ola");
+        cachorro.reagir(19);
+        cachorro.reagir(2, 5);
+//
+//        lobo.emitirSom();
+//
+//        reptil.setPeso(5);
+//        reptil.setIdade(3);
+//        reptil.setMembros("4");
+//        reptil.setCorEscama("Laranja");
+//        reptil.emitirSom();
+//
+//        peixe.setPeso(2);
+//        peixe.setIdade(1);
+//        peixe.setMembros("7 nadadeira");
+//        peixe.setCorEscama("Azul marinho");
+//        peixe.locomover();
+//        peixe.soltarBolha();
+//
+//        ave.setPeso(12);
+//        ave.setIdade(4);
+//        ave.setMembros("4");
+//        ave.setCorPena("Vermelho");
+//        ave.locomover();
+//        ave.fazerNinho();
     }
 }
---
+++ animal/Lobo.java
@@ -0,0 +1,9 @@
+package animal;
+
+public class Lobo extends Mamifero {
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Auuuuuuuuuuu!");
+    }
+}
