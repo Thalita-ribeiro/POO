7d35be1e3da6a82b17c0a34d014f66615181f9d8
Thalita Ribeiro <sribeiro.thalita@gmail.com>
2021-10-28 21:03:25 -0300

    Foi criado a classe funcionario

---
+++ funcionario/Funcionario.java
@@ -0,0 +1,37 @@
+package funcionario;
+
+public class Funcionario {
+    protected String nome;
+    private String cpf;
+    private double salario;
+
+    public Funcionario(String nome, String cpf, double salario) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.salario = salario;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getBonificacao() {
+        return this.salario * 1.2;
+    }
+}
