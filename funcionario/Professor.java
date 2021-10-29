ccbbc357c9e4fa13d20503a2b3af49745970fdbf
Thalita Ribeiro <sribeiro.thalita@gmail.com>
2021-10-28 21:31:21 -0300

    Criei a classe Professor

--- funcionario/Professor.java
+++ funcionario/Professor.java
@@ -1,2 +1,13 @@
-package funcionario;public class Professor {
+package funcionario;
+
+public class Professor extends Funcionario {
+
+    public Professor(String nome, String cpf, double salario) {
+        super(nome, cpf, salario);
+    }
+
+    @Override
+    public double getBonificacao() {
+        return super.getSalario() * 1.5;
+    }
 }
