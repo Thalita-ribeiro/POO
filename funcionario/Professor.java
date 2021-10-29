c004f5e18d57d7a38f9dad90b75ed9fec78fa76a
Thalita Ribeiro <sribeiro.thalita@gmail.com>
2021-10-28 21:41:46 -0300

    Adicionei o metodo corrigir prova

--- funcionario/Professor.java
+++ funcionario/Professor.java
@@ -1,6 +1,7 @@
 package funcionario;
 
 public class Professor extends Funcionario {
+    private boolean corrigiuAsProvas;
 
     public Professor(String nome, String cpf, double salario) {
         super(nome, cpf, salario);
@@ -9,5 +10,9 @@ public class Professor extends Funcionario {
     @Override
     public double getBonificacao() {
         return super.getSalario() * 1.5;
+    }
+
+    public void corrigirProva() {
+        System.out.println("Corrigiu as provas dos alunos? " + true);
     }
 }
