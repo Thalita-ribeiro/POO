5f27d57372c891aa5f77c41600638d174082d9e8
Thalita Ribeiro <sribeiro.thalita@gmail.com>
2021-10-28 21:28:51 -0300

    Testando a minha classe Professor

---
+++ funcionario/Test.java
@@ -0,0 +1,11 @@
+package funcionario;
+
+public class Test {
+    public static void main(String[] args) {
+        Professor professor = new Professor("Thalita", "4904557811", 12000);
+        professor.getBonificacao();
+
+        System.out.println(professor.getNome() + ", seu salario é de: " + professor.getSalario());
+        System.out.println(professor.getNome() + ", sua bonificacao é de: " + professor.getBonificacao());
+    }
+}
