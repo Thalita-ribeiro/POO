51440004566ad5f98687fca0a44faffba6e00a91
Thalita Ribeiro <sribeiro.thalita@gmail.com>
2021-11-03 21:34:22 -0300

    Foi tirando os getters e setters não utilizado

--- funcionario/Funcionario.java
+++ funcionario/Funcionario.java
@@ -11,24 +11,12 @@ public class Funcionario {
         this.salario = salario;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
+    public String getNome() {
+        return nome;
     }
 
     public double getSalario() {
         return salario;
-    }
-
-    public void setSalario(double salario) {
-        this.salario = salario;
     }
 
     public double getBonificacao() {
