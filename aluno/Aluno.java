9aeea5862f83ed3e06ef053747ac413c978f3b36
Thalita Ribeiro <sribeiro.thalita@gmail.com>
2021-12-07 22:59:58 -0300

    Calcular media de aluno

---
+++ aluno/Aluno.java
@@ -0,0 +1,9 @@
+package aluno;
+
+public class Aluno {
+   private String nome;
+
+    public double culcularMedia(double nota1, double nota2) {
+        return (nota1 + nota1) / 2;
+    }
+}
---
+++ aluno/CalcularMedia.java
@@ -0,0 +1,2 @@
+package aluno;public class CalcularMedia {
+}
