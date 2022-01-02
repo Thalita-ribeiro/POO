cfb89644c12630578387c8ec5c124d6cbf0eee1f
Thalita Ribeiro <sribeiro.thalita@gmail.com>
2022-01-01 22:10:04 -0300

    testing the application

--- Calcular/Application.java
+++ Calcular/Application.java
@@ -1,2 +1,9 @@
-package Calcular;public class application {
+package Calcular;
+
+public class Application {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        System.out.println(calculator.calculator(0, 10));
+    }
 }
